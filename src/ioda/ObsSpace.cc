12020f8dd5a363095a9333f85c48c082ca0eacf3
Guillaume Vernieres <guillaume.vernieres@gmail.com>
2018-06-03 22:15:05 -0600

    Refactoring of Jili's ioda adt.

--- src/ioda/ObsSpace.cc
+++ src/ioda/ObsSpace.cc
@@ -37,7 +37,9 @@ ObsSpace::ObsSpace(const eckit::Configuration & config,
   else if (obsname_ == "SeaIceThickness")
     ioda_obsdb_seaicethick_setup_f90(keyOspace_, &configc);
   else if (obsname_ == "InsituTemperature")
-    ioda_obsdb_insitutemperature_setup_f90(keyOspace_, &configc);  
+    ioda_obsdb_insitutemperature_setup_f90(keyOspace_, &configc);
+  else if (obsname_ == "ADT")
+    ioda_obsdb_adt_setup_f90(keyOspace_, &configc);    
   else
     ioda_obsdb_setup_f90(keyOspace_, &configc);
 
@@ -54,7 +56,9 @@ ObsSpace::~ObsSpace() {
   else if (obsname_ == "SeaIceThickness")
     ioda_obsdb_seaicethick_delete_f90(keyOspace_);
   else if (obsname_ == "InsituTemperature")
-    ioda_obsdb_insitutemperature_delete_f90(keyOspace_);  
+    ioda_obsdb_insitutemperature_delete_f90(keyOspace_);
+  else if (obsname_ == "ADT")
+    ioda_obsdb_adt_delete_f90(keyOspace_);    
   else
     ioda_obsdb_delete_f90(keyOspace_);
 }
@@ -69,7 +73,9 @@ void ObsSpace::getdb(const std::string & col, int & keyData) const {
   else if (obsname_ == "SeaIceThickness")
     ioda_obsdb_seaicethick_get_f90(keyOspace_, col.size(), col.c_str(), keyData);  
   else if (obsname_ == "InsituTemperature")
-    ioda_obsdb_insitutemperature_get_f90(keyOspace_, col.size(), col.c_str(), keyData);  
+    ioda_obsdb_insitutemperature_get_f90(keyOspace_, col.size(), col.c_str(), keyData);
+  else if (obsname_ == "ADT")
+    ioda_obsdb_adt_get_f90(keyOspace_, col.size(), col.c_str(), keyData);  
   else
     ioda_obsdb_get_f90(keyOspace_, col.size(), col.c_str(), keyData);
 }
@@ -93,7 +99,9 @@ Locations * ObsSpace::locations(const util::DateTime & t1, const util::DateTime 
   else if (obsname_ == "SeaIceThickness")
     ioda_obsdb_seaicethick_getlocations_f90(keyOspace_, &p1, &p2, keylocs);
   else if (obsname_ == "InsituTemperature")
-    ioda_obsdb_insitutemperature_getlocations_f90(keyOspace_, &p1, &p2, keylocs);  
+    ioda_obsdb_insitutemperature_getlocations_f90(keyOspace_, &p1, &p2, keylocs);
+  else if (obsname_ == "ADT")
+    ioda_obsdb_adt_getlocations_f90(keyOspace_, &p1, &p2, keylocs);    
   else
     ioda_obsdb_getlocations_f90(keyOspace_, &p1, &p2, keylocs);
 
@@ -111,7 +119,9 @@ int ObsSpace::nobs() const {
   else if (obsname_ == "SeaIceThickness")
     ioda_obsdb_seaicethick_nobs_f90(keyOspace_, n);
   else if (obsname_ == "InsituTemperature")
-    ioda_obsdb_insitutemperature_nobs_f90(keyOspace_, n);  
+    ioda_obsdb_insitutemperature_nobs_f90(keyOspace_, n);
+  else if (obsname_ == "ADT")
+    ioda_obsdb_adt_nobs_f90(keyOspace_, n);    
   else
     ioda_obsdb_nobs_f90(keyOspace_, n);
 
@@ -132,7 +142,9 @@ void ObsSpace::generateDistribution(const eckit::Configuration & conf) {
   else if (obsname_ == "SeaIceThickness")
     ioda_obsdb_seaicethick_generate_f90(keyOspace_, &configc, &p1, &p2);
   else if (obsname_ == "InsituTemperature")
-    ioda_obsdb_insitutemperature_generate_f90(keyOspace_, &configc, &p1, &p2);  
+    ioda_obsdb_insitutemperature_generate_f90(keyOspace_, &configc, &p1, &p2);
+  else if (obsname_ == "ADT")
+    ioda_obsdb_adt_generate_f90(keyOspace_, &configc, &p1, &p2);    
   else
     ioda_obsdb_generate_f90(keyOspace_, &configc, &p1, &p2);
 
