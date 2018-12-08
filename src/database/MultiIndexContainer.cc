55e40be8d48d70261e52a874e05e5c554553e868
Xin Zhang <xin.l.zhang@noaa.gov>
2018-12-08 10:51:03 -0700

    move multiindexcontainer to database directory

--- src/database/MultiIndexContainer.cc
+++ src/database/MultiIndexContainer.cc
@@ -5,7 +5,7 @@
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-#include "ioda/MultiIndexContainer.h"
+#include "database/MultiIndexContainer.h"
 
 namespace ioda {
 // -----------------------------------------------------------------------------
