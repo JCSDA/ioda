9b2ce6547a2e6f32ca09bd799a2c23bc6025e77d
Xin Zhang <xin.l.zhang@noaa.gov>
2018-12-23 09:23:05 -0700

    Add get_db dataetime C++ interface

--- src/database/MultiIndexContainer.cc
+++ src/database/MultiIndexContainer.cc
@@ -55,33 +55,148 @@ namespace ioda {
   }
 // -----------------------------------------------------------------------------
 
-bool ObsSpaceContainer::has(const std::string & group, const std::string & variable) const {
-  auto var = DataContainer.find(boost::make_tuple(group, variable));
-  return (var != DataContainer.end());
-}
+  void ObsSpaceContainer::inquire(const std::string & group, const std::string & variable,
+                                  const std::size_t vsize, double vdata[]) const {
+    if (has(group, variable)) {  // Found the required record in database
+      auto var = DataContainer.find(boost::make_tuple(group, variable));
+      const std::type_info & typeInput = var->data.get()->type();
+      if (typeInput == typeid(float)) {
+        oops::Log::debug() << " DataContainer::inquire: inconsistent type : "
+                           << " From float to double on "
+                           << variable << " @ " << group << std::endl;
+        for (std::size_t ii = 0; ii < vsize; ++ii) {
+          float zz = boost::any_cast<float>(var->data.get()[ii]);
+          if (zz == missingValue<float>()) {
+            vdata[ii] = missingValue<double>();
+          } else {
+            vdata[ii] = static_cast<double>(zz);
+          }
+        }
+      } else if (typeInput == typeid(int)) {
+          oops::Log::debug() << " DataContainer::inquire: inconsistent type : "
+                             << " From int to double on "
+                             << variable << " @ " << group << std::endl;
+          for (std::size_t ii = 0; ii < vsize; ++ii) {
+            int zz = boost::any_cast<int>(var->data.get()[ii]);
+            if (zz == missingValue<int>()) {
+              vdata[ii] = missingValue<double>();
+            } else {
+              vdata[ii] = static_cast<double>(zz);
+            }
+          }
+      } else {  // the in/out types should be the same
+        ASSERT(typeInput == typeid(double));
+        for (std::size_t ii = 0; ii < vsize; ++ii)
+          vdata[ii] = boost::any_cast<double>(var->data.get()[ii]);
+      }
+    } else {  // Required record is not found
+      std::string ErrorMsg =
+             "DataContainer::inquire: " + variable + " @ " + group +" is not found";
+      ABORT(ErrorMsg);
+    }
+  }
+
+  void ObsSpaceContainer::inquire(const std::string & group, const std::string & variable,
+                                  const std::size_t vsize, float vdata[]) const {
+    if (has(group, variable)) {  // Found the required record in database
+      auto var = DataContainer.find(boost::make_tuple(group, variable));
+      const std::type_info & typeInput = var->data.get()->type();
+      ASSERT(typeInput == typeid(float));
+      for (std::size_t ii = 0; ii < vsize; ++ii)
+        vdata[ii] = boost::any_cast<float>(var->data.get()[ii]);
+    } else {  // Required record is not found
+      std::string ErrorMsg =
+             "DataContainer::inquire: " + variable + " @ " + group +" is not found";
+      ABORT(ErrorMsg);
+    }
+  }
+
+  void ObsSpaceContainer::inquire(const std::string & group, const std::string & variable,
+                                  const std::size_t vsize, int vdata[]) const {
+    if (has(group, variable)) {  // Found the required record in database
+      auto var = DataContainer.find(boost::make_tuple(group, variable));
+      const std::type_info & typeInput = var->data.get()->type();
+      if (typeInput == typeid(double)) {
+          oops::Log::debug() << " DataContainer::inquire: inconsistent type : "
+                             << " From double to int on "
+                             << variable << " @ " << group << std::endl;
+          for (std::size_t ii = 0; ii < vsize; ++ii) {
+            double zz = boost::any_cast<double>(var->data.get()[ii]);
+            if (zz == missingValue<double>()) {
+              vdata[ii] = missingValue<int>();
+            } else {
+              vdata[ii] = static_cast<int>(zz);
+            }
+          }
+      } else {  // the in/out types should be the same
+        ASSERT(typeInput == typeid(int));
+        for (std::size_t ii = 0; ii < vsize; ++ii)
+          vdata[ii] = boost::any_cast<int>(var->data.get()[ii]);
+      }
+    } else {  // Required record is not found
+      std::string ErrorMsg =
+             "DataContainer::inquire: " + variable + " @ " + group +" is not found";
+      ABORT(ErrorMsg);
+    }
+  }
+
+  void ObsSpaceContainer::inquire(const std::string & group, const std::string & variable,
+                                  const std::size_t vsize, util::DateTime vdata[]) const {
+    if (has(group, "date") && has(group, "time")) {  // Found the required record in database
+      auto date = DataContainer.find(boost::make_tuple(group, "date"));
+      auto time = DataContainer.find(boost::make_tuple(group, "time"));
+      const std::type_info & date_type = date->data.get()->type();
+      const std::type_info & time_type = time->data.get()->type();
+      ASSERT(date_type == typeid(int));
+      ASSERT(time_type == typeid(int));
+      int vdate, vtime;
+      for (std::size_t ii = 0; ii < vsize; ++ii) {
+        vdate = boost::any_cast<int>(date->data.get()[ii]);
+        vtime = boost::any_cast<int>(time->data.get()[ii]);
+        util::DateTime tmp(static_cast<int>(vdate/10000),
+                           static_cast<int>((vdate%10000)/100),
+                           static_cast<int>((vdate%10000)%100),
+                           static_cast<int>(vtime/10000),
+                           static_cast<int>((vtime%10000)/100),
+                           static_cast<int>((vtime%10000)%100));
+        vdata[ii] = tmp;
+      }
+    } else {  // Required record is not found
+      std::string ErrorMsg =
+             "DataContainer::inquire: " + variable + " @ " + group +" is not found";
+      ABORT(ErrorMsg);
+    }
+  }
 
 // -----------------------------------------------------------------------------
 
-void ObsSpaceContainer::dump(const std::string & file_name) const {
-  // Open the file for output
-  std::unique_ptr<ioda::IodaIO> fileio
-    {ioda::IodaIOfactory::Create(file_name, "W", windowStart(), windowEnd(),
-    comm(), nlocs(), 0, 0, nvars())};  //  Not sure nrecs and nobs are useful
-
-  // List all records and write out the every record
-  auto & var = DataContainer.get<ObsSpaceContainer::by_variable>();
-  for (auto iter = var.begin(); iter != var.end(); ++iter)
-    fileio->WriteVar_any(iter->variable + "@" + iter->group, iter->data.get());
-}
+  bool ObsSpaceContainer::has(const std::string & group, const std::string & variable) const {
+    auto var = DataContainer.find(boost::make_tuple(group, variable));
+    return (var != DataContainer.end());
+  }
 
 // -----------------------------------------------------------------------------
 
-void ObsSpaceContainer::print(std::ostream & os) const {
-  auto & var = DataContainer.get<ObsSpaceContainer::by_variable>();
-  os << "ObsSpace Multi.Index Container for IODA" << "\n";
-  for (auto iter = var.begin(); iter != var.end(); ++iter)
-    os << iter->variable << " @ " << iter->group << "\n";
-}
+  void ObsSpaceContainer::dump(const std::string & file_name) const {
+    // Open the file for output
+    std::unique_ptr<ioda::IodaIO> fileio
+      {ioda::IodaIOfactory::Create(file_name, "W", windowStart(), windowEnd(),
+      comm(), nlocs(), 0, 0, nvars())};  //  Not sure nrecs and nobs are useful
+
+    // List all records and write out the every record
+    auto & var = DataContainer.get<ObsSpaceContainer::by_variable>();
+    for (auto iter = var.begin(); iter != var.end(); ++iter)
+      fileio->WriteVar_any(iter->variable + "@" + iter->group, iter->data.get());
+  }
+
+// -----------------------------------------------------------------------------
+
+  void ObsSpaceContainer::print(std::ostream & os) const {
+    auto & var = DataContainer.get<ObsSpaceContainer::by_variable>();
+    os << "ObsSpace Multi.Index Container for IODA" << "\n";
+    for (auto iter = var.begin(); iter != var.end(); ++iter)
+      os << iter->variable << " @ " << iter->group << "\n";
+  }
 
 // -----------------------------------------------------------------------------
 
