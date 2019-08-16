2ebdbd556e5e9a52a6ebf29f6ac475faf42309b7
Stephen Herbener <stephen.herbener@gmail.com>
2019-08-16 17:34:05 -0600

    General cleanup of ObsSpace code. All tests are passing and obs grouping is working!

--- src/ioda/ObsSpace.h
+++ src/ioda/ObsSpace.h
@@ -66,22 +66,22 @@ class ObsSpace : public oops::ObsSpaceBase {
   bool has(const std::string &, const std::string &) const;
 
   void get_db(const std::string & group, const std::string & name,
-              const size_t & vsize, int vdata[]) const;
+              const std::size_t vsize, int vdata[]) const;
   void get_db(const std::string & group, const std::string & name,
-              const size_t & vsize, float vdata[]) const;
+              const std::size_t vsize, float vdata[]) const;
   void get_db(const std::string & group, const std::string & name,
-              const size_t & vsize, double vdata[]) const;
+              const std::size_t vsize, double vdata[]) const;
   void get_db(const std::string & group, const std::string & name,
-              const size_t & vsize, util::DateTime vdata[]) const;
+              const std::size_t vsize, util::DateTime vdata[]) const;
 
   void put_db(const std::string & group, const std::string & name,
-              const size_t & vsize, const int vdata[]);
+              const std::size_t vsize, const int vdata[]);
   void put_db(const std::string & group, const std::string & name,
-              const size_t & vsize, const float vdata[]);
+              const std::size_t vsize, const float vdata[]);
   void put_db(const std::string & group, const std::string & name,
-              const size_t & vsize, const double vdata[]);
+              const std::size_t vsize, const double vdata[]);
   void put_db(const std::string & group, const std::string & name,
-              const size_t & vsize, const util::DateTime vdata[]);
+              const std::size_t vsize, const util::DateTime vdata[]);
 
   /*! \details This method will return the name of the obs type being stored */
   const std::string & obsname() const {return obsname_;}
@@ -107,46 +107,47 @@ class ObsSpace : public oops::ObsSpaceBase {
   void InitFromFile(const std::string & filename);
   void GenMpiDistribution(const std::unique_ptr<IodaIO> & FileIO);
   void GenRecordNumbers(const std::unique_ptr<IodaIO> & FileIO,
-                        std::vector<std::size_t> & Records);
+                        std::vector<std::size_t> & Records) const;
   template<typename DATATYPE>
-  void GenRnumsFromVar(const std::vector<DATATYPE> & VarData,
-                       std::vector<std::size_t> & Records);
+  static void GenRnumsFromVar(const std::vector<DATATYPE> & VarData,
+                              std::vector<std::size_t> & Records);
   void ApplyTimingWindow(const std::unique_ptr<IodaIO> & FileIO);
 
   template<typename VarType>
   void ApplyDistIndex(std::unique_ptr<VarType[]> & FullData,
                       const std::vector<std::size_t> & FullShape,
                       std::unique_ptr<VarType[]> & IndexedData,
-                      std::vector<std::size_t> & IndexedShape, std::size_t & IndexedSize);
+                      std::vector<std::size_t> & IndexedShape,
+                      std::size_t & IndexedSize) const;
 
-  std::string DesiredVarType(std::string & GroupName, std::string & FileVarType);
+  static std::string DesiredVarType(std::string & GroupName, std::string & FileVarType);
 
   // Convert variable data types including the missing value marks
   template<typename FromType, typename ToType>
-  void ConvertVarType(const FromType * FromVar, ToType * ToVar,
-                      const std::size_t & VarSize) const;
+  static void ConvertVarType(const FromType * FromVar, ToType * ToVar,
+                             const std::size_t VarSize);
 
   // Dump the database into the output file
   void SaveToFile(const std::string & file_name);
 
   // Methods for tranferring data from the database into a variable.
   template <typename DATATYPE>
-  void get_db_helper(const std::string &, const std::string &,
-                     const std::size_t &, DATATYPE[]) const;
+  void get_db_helper(const std::string & group, const std::string & name,
+                     const std::size_t vsize, DATATYPE vdata[]) const;
 
   template<typename DbType, typename VarType>
   void LoadFromDbConvert(const std::string & GroupName, const std::string & VarName,
-                    const std::vector<std::size_t> & VarShape, const std::size_t & VarSize,
+                    const std::vector<std::size_t> & VarShape, const std::size_t VarSize,
                     VarType * VarData) const;
 
   // Methods for tranferring data from a variable into the database.
   template <typename DATATYPE>
-  void put_db_helper(const std::string &, const std::string &,
-                     const std::size_t &, const DATATYPE[]);
+  void put_db_helper(const std::string & group, const std::string & name,
+                     const std::size_t vsize, const DATATYPE vdata[]);
 
   template<typename VarType, typename DbType>
   void ConvertStoreToDb(const std::string & GroupName, const std::string & VarName,
-                    const std::vector<std::size_t> & VarShape, const std::size_t & VarSize,
+                    const std::vector<std::size_t> & VarShape, const std::size_t VarSize,
                     const VarType * VarData);
 
   /*! \brief name of obs space */
@@ -204,8 +205,8 @@ class ObsSpace : public oops::ObsSpaceBase {
 
 /*! \brief Specialized (for DateTime type) helper function for public get_db */
 template <>
-void ObsSpace::get_db_helper<util::DateTime>(const std::string &, const std::string &,
-                                             const std::size_t &, util::DateTime[]) const;
+void ObsSpace::get_db_helper<util::DateTime>(const std::string & group,
+           const std::string & name, const std::size_t vsize, util::DateTime vdata[]) const;
 
 }  // namespace ioda
 
