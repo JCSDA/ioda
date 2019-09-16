29e65ce29315b77563b44e00295047c583f8d78c
jswhit <Jeffrey.S.Whitaker@noaa.gov>
2019-09-16 20:59:25 +0000

    create boilerplate ObsSpaceView objects

---
+++ src/ioda/ObsSpaceView.cc
@@ -0,0 +1,285 @@
+/*
+ * (C) Copyright 2017-2019 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "ioda/ObsSpaceView.h"
+
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define BOOST_STACKTRACE_GNU_SOURCE_NOT_REQUIRED
+#include <boost/stacktrace.hpp>
+
+#include "eckit/config/Configuration.h"
+#include "eckit/exception/Exceptions.h"
+
+#include "ioda/ObsSpace.h"
+
+#include "oops/parallel/mpi/mpi.h"
+#include "oops/util/abor1_cpp.h"
+#include "oops/util/DateTime.h"
+#include "oops/util/Duration.h"
+#include "oops/util/Logger.h"
+#include "oops/util/Random.h"
+
+#include "distribution/DistributionFactory.h"
+#include "fileio/IodaIOfactory.h"
+
+namespace ioda {
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details Config based constructor for an ObsSpaceView object. This constructor will read
+ *          in from the obs file and transfer the variables into the obs container. Obs
+ *          falling outside the DA timing window, specified by bgn and end, will be
+ *          discarded before storing them in the obs container.
+ *
+ * \param[in] config ECKIT configuration segment holding obs types specs
+ * \param[in] bgn    DateTime object holding the start of the DA timing window
+ * \param[in] end    DateTime object holding the end of the DA timing window
+ */
+ObsSpaceView::ObsSpaceView(const eckit::Configuration & config,
+                   const util::DateTime & bgn, const util::DateTime & end)
+  : oops::ObsSpaceBase(config, bgn, end),
+    obsspace_ (new ObsSpace(config, bgn, end))
+
+  {}
+// -----------------------------------------------------------------------------
+/*!
+ * \details Destructor for an ObsSpace object. This destructor will clean up the ObsSpace
+ *          object and optionally write out the contents of the obs container into
+ *          the output file. The save-to-file operation is invoked when an output obs
+ *          file is specified in the ECKIT configuration segment associated with the
+ *          ObsSpace object.
+ */
+ObsSpaceView::~ObsSpaceView() {
+}
+
+void ObsSpaceView::get_db(const std::string & group, const std::string & name,
+                      const std::size_t vsize, int vdata[]) const {
+  obsspace_->get_db(group, name, vsize, vdata);
+}
+
+void ObsSpaceView::get_db(const std::string & group, const std::string & name,
+                      const std::size_t vsize, float vdata[]) const {
+  obsspace_->get_db(group, name, vsize, vdata);
+}
+
+void ObsSpaceView::get_db(const std::string & group, const std::string & name,
+                      const std::size_t vsize, double vdata[]) const {
+  obsspace_->get_db(group, name, vsize, vdata);
+}
+
+void ObsSpaceView::get_db(const std::string & group, const std::string & name,
+                      const std::size_t vsize, util::DateTime vdata[]) const {
+  obsspace_->get_db(group, name, vsize, vdata);
+}
+
+void ObsSpaceView::put_db(const std::string & group, const std::string & name,
+                      const std::size_t vsize, const int vdata[]) {
+  obsspace_->put_db(group, name, vsize, vdata);
+}
+
+void ObsSpaceView::put_db(const std::string & group, const std::string & name,
+                      const std::size_t vsize, const float vdata[]) {
+  obsspace_->put_db(group, name, vsize, vdata);
+}
+
+void ObsSpaceView::put_db(const std::string & group, const std::string & name,
+                      const std::size_t vsize, const double vdata[]) {
+  obsspace_->put_db(group, name, vsize, vdata);
+}
+
+void ObsSpaceView::put_db(const std::string & group, const std::string & name,
+                      const std::size_t vsize, const util::DateTime vdata[]) {
+  obsspace_->put_db(group, name, vsize, vdata);
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method checks for the existence of the group, name combination
+ *          in the obs container. If the combination exists, "true" is returned,
+ *          otherwise "false" is returned.
+ */
+
+bool ObsSpaceView::has(const std::string & group, const std::string & name) const {
+  return obsspace_->has(group, name);
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns the number of unique locations in the input
+ *          obs file. Note that nlocs from the obs container may be smaller
+ *          than nlocs from the input obs file due to the removal of obs outside
+ *          the DA timing window and/or due to distribution of obs across
+ *          multiple process elements.
+ */
+std::size_t ObsSpaceView::gnlocs() const {
+  return obsspace_->gnlocs();
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns the number of unique locations in the obs
+ *          container. Note that nlocs from the obs container may be smaller
+ *          than nlocs from the input obs file due to the removal of obs outside
+ *          the DA timing window and/or due to distribution of obs across
+ *          multiple process elements.
+ */
+std::size_t ObsSpaceView::nlocs() const {
+  return obsspace_->nlocs();
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns the number of unique records in the obs
+ *          container. A record is an atomic unit of locations that belong
+ *          together such as a single radiosonde sounding.
+ */
+std::size_t ObsSpaceView::nrecs() const {
+  return obsspace_->nrecs();
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns the number of unique variables in the obs
+ *          container. "Variables" refers to the quantities that can be
+ *          assimilated as opposed to meta data.
+ */
+std::size_t ObsSpaceView::nvars() const {
+  return obsspace_->nvars();
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns a reference to the record number vector
+ *          data member. This is for read only access.
+ */
+const std::vector<std::size_t> & ObsSpaceView::recnum() const {
+  return obsspace_->recnum();
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns a reference to the index vector
+ *          data member. This is for read only access.
+ */
+const std::vector<std::size_t> & ObsSpaceView::index() const {
+  return obsspace_->index();
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns the begin iterator associated with the
+ *          recidx_ data member.
+ */
+const ObsSpaceView::RecIdxIter ObsSpaceView::recidx_begin() const {
+  return obsspace_->recidx_begin();
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns the end iterator associated with the
+ *          recidx_ data member.
+ */
+const ObsSpaceView::RecIdxIter ObsSpaceView::recidx_end() const {
+  return obsspace_->recidx_end();
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns a boolean value indicating whether the
+ *          given record number exists in the recidx_ data member.
+ */
+bool ObsSpaceView::recidx_has(const std::size_t RecNum) const {
+  return obsspace_->recidx_has(RecNum);
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns the current record number, pointed to by the
+ *          given iterator, from the recidx_ data member.
+ */
+std::size_t ObsSpaceView::recidx_recnum(const RecIdxIter & Irec) const {
+  return obsspace_->recidx_recnum(Irec);
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns the current vector, pointed to by the
+ *          given iterator, from the recidx_ data member.
+ */
+const std::vector<std::size_t> & ObsSpaceView::recidx_vector(const RecIdxIter & Irec) const {
+  return obsspace_->recidx_vector(Irec);
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns the current vector, pointed to by the
+ *          given iterator, from the recidx_ data member.
+ */
+const std::vector<std::size_t> & ObsSpaceView::recidx_vector(const std::size_t RecNum) const {
+  return obsspace_->recidx_vector(RecNum);
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method returns the all of the record numbers from the
+ *          recidx_ data member (ie, all the key values) in a vector.
+ */
+std::vector<std::size_t> ObsSpaceView::recidx_all_recnums() const {
+  return obsspace_->recidx_all_recnums();
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method will generate a set of latitudes and longitudes of which
+ *          can be used for testing without reading in an obs file. Two latitude
+ *          values, two longitude values, the number of locations (nobs keyword)
+ *          and an optional random seed are specified in the configuration given
+ *          by the conf parameter. Random locations between the two latitudes and
+ *          two longitudes are generated and stored in the obs container as meta data.
+ *          Random time stamps that fall inside the given timing window (which is
+ *          specified in the configuration file) are alos generated and stored
+ *          in the obs container as meta data.
+ *
+ * \param[in] conf ECKIT configuration segment built from an input configuration file.
+ */
+void ObsSpaceView::generateDistribution(const eckit::Configuration & conf) {
+  obsspace_->generateDistribution(conf);
+}
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method provides a way to print an ObsSpace object in an output
+ *          stream. It simply prints a dummy message for now.
+ */
+void ObsSpaceView::print(std::ostream & os) const {
+  os << obsspace_;
+}
+
+
+// -----------------------------------------------------------------------------
+/*!
+ * \details This method provides a means for printing Jo in
+ *          an output stream. For now a dummy message is printed.
+ */
+void ObsSpaceView::printJo(const ObsVector & dy, const ObsVector & grad) {
+  obsspace_->printJo(dy, grad);
+}
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ioda
