36eb6622cda65bb50177067c903f20667b80bd5a
Anna Shlyaeva <anna.v.shlyaeva@noaa.gov>
2019-10-14 21:53:13 +0000

    replace char[] with char*

--- src/ioda/obsspace_f.h
+++ src/ioda/obsspace_f.h
@@ -26,11 +26,11 @@ extern "C" {
   int obsspace_get_nlocs_f(const ObsSpace &);
   int obsspace_get_nrecs_f(const ObsSpace &);
   int obsspace_get_nvars_f(const ObsSpace &);
-  void obsspace_get_comm_f(const ObsSpace &, int &, char[]);
+  void obsspace_get_comm_f(const ObsSpace &, int &, char *);
   void obsspace_get_recnum_f(const ObsSpace &, const std::size_t &, std::size_t *);
   void obsspace_get_index_f(const ObsSpace &, const std::size_t &, std::size_t *);
 
-  void obsspace_obsname_f(const ObsSpace &, const size_t &, char[]);
+  void obsspace_obsname_f(const ObsSpace &, const size_t &, char *);
 
   bool obsspace_has_f(const ObsSpace &, const char *, const char *);
 
