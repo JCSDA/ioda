8b759b82c7b0b28a585488c2a3759a6a56a2a86b
Travis Sluka <tsluka@ucar.edu>
2020-10-15 17:45:38 -0600

    Fix use of QC flags with LETKF (#489)
    * add missingValue to ObsVector
    
    * ObsLocGC99::multiply obeys missing values
    
    * fix coding norms
    
    * add packEigen to ObsVector

--- src/ObsVector.h
+++ src/ObsVector.h
@@ -8,6 +8,7 @@
 #ifndef OBSVECTOR_H_
 #define OBSVECTOR_H_
 
+#include <Eigen/Dense>
 #include <ostream>
 #include <string>
 #include <vector>
@@ -58,6 +59,7 @@ class ObsVector : public util::Printable,
   const double & operator[](const std::size_t ii) const {return values_.at(ii);}
   double & operator[](const std::size_t ii) {return values_.at(ii);}
   unsigned int nobs() const;  // Number of active observations (missing values not included)
+  Eigen::VectorXd  packEigen() const;
 
   const double & toFortran() const;
   double & toFortran();
