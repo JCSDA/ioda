d7a678b0d5bb45e942b406af865f8b02aa212689
Travis Elless <113720457+TravisElless-NOAA@users.noreply.github.com>
2025-02-06 11:58:35 -0500

    Add missing file handling for BUFR data (#1422)
    * Add missing file handling for BUFR data
    
    * update comment
    
    * remove space

--- src/engines/ioda/src/ioda/Engines/ReadBufrFile.cpp
+++ src/engines/ioda/src/ioda/Engines/ReadBufrFile.cpp
@@ -12,6 +12,7 @@
 #include "oops/util/missingValues.h"
 
 #include "ioda/Engines/ReadBufrFile.h"
+#include "ioda/Exception.h"
 
 namespace ioda {
 namespace Engines {
@@ -26,37 +27,70 @@ namespace Engines {
       : ReaderBase(createParams), fileName_(params.fileName)
   {
     oops::Log::trace() << "ioda::Engines::ReadBufrFile start constructor" << std::endl;
+    if (haveFileReadAccess(fileName_)) {
+    	// Create an in-memory backend
+    	Engines::BackendNames backendName = Engines::BackendNames::ObsStore;
+    	Engines::BackendCreationParameters backendParams;
+    	Group backend = constructBackend(backendName, backendParams);
 
-    // Create an in-memory backend
-    Engines::BackendNames backendName = Engines::BackendNames::ObsStore;
-    Engines::BackendCreationParameters backendParams;
-    Group backend = constructBackend(backendName, backendParams);
+    	// Load the BUFR file into the backend
+    	Engines::Bufr::Bufr_Parameters bufrparams;
+    	bufrparams.filename = params.fileName;
+    	bufrparams.mappingFile = params.mappingFile;
 
-    // Load the BUFR file into the backend
-    Engines::Bufr::Bufr_Parameters bufrparams;
-    bufrparams.filename = params.fileName;
-    bufrparams.mappingFile = params.mappingFile;
+    	if (params.tablePath.value())
+    	{
+      	  bufrparams.tablePath = params.tablePath.value().get();
+    	}
 
-    if (params.tablePath.value())
-    {
-      bufrparams.tablePath = params.tablePath.value().get();
+    	if (params.category.value())
+    	{
+      	  bufrparams.category = params.category.value().get();
+    	}
+
+    	if (params.cacheCategories.value())
+    	{
+      	  bufrparams.cacheCategories = params.cacheCategories.value().get();
+    	}
+
+    	obs_group_ = Engines::Bufr::openFile(bufrparams, backend);
+
+    	oops::Log::trace() << "ioda::Engines::ReadBufrFile end constructor" << std::endl;
+  } else{
+        // Input file does not exist (is not readable actually)
+        if (params.missingFileAction.value() == "warn") {
+            oops::Log::info() << "WARNING: input file is not readable, "
+               << "will continue with empty file representation" << std::endl
+               << "WARNING:     file: " << fileName_ << std::endl;
+            // Create a memory backend as a placehold for the missing file. Make the
+            // memory backend look like an empty file (Location == 0).
+            Engines::BackendNames backendName = Engines::BackendNames::ObsStore;
+            Engines::BackendCreationParameters backendParams;
+            Group backend = constructBackend(backendName, backendParams);
+
+            // Create the ObsGroup and attach the backend.
+    	    // Load the BUFR file into the backend
+    	    Engines::Bufr::Bufr_Parameters bufrparams;
+    	    bufrparams.filename = params.fileName;
+     	    bufrparams.mappingFile = params.mappingFile;
+
+            obs_group_ = ObsGroup::generate(backend, {});
+
+            // Create the Location dimension and set its size to zero.
+            obs_group_.vars.create<int64_t>("Location", { 0 })
+                .setIsDimensionScale("Location");
+        } else if (params.missingFileAction.value() == "error") {
+            std::string ErrMsg = std::string("Input file is not readable, ") +
+                std::string("will stop execution. File: ") + fileName_ + std::string("\n");
+            throw Exception(ErrMsg, ioda_Here());
+        } else {
+            std::string ErrMsg = std::string("Unrecognized input file missing action: ") +
+                params.missingFileAction.value();
+            throw Exception(ErrMsg, ioda_Here());
+        }
+  
     }
-
-    if (params.category.value())
-    {
-      bufrparams.category = params.category.value().get();
-    }
-
-    if (params.cacheCategories.value())
-    {
-      bufrparams.cacheCategories = params.cacheCategories.value().get();
-    }
-
-    obs_group_ = Engines::Bufr::openFile(bufrparams, backend);
-
-    oops::Log::trace() << "ioda::Engines::ReadBufrFile end constructor" << std::endl;
   }
-
   void ReadBufrFile::print(std::ostream & os) const
   {
     os << fileName_;
