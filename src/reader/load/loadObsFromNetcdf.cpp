a0c246cb3534b4f7265445ce3f5a13b9c2d836f6
Stephen Herbener <32968781+srherbener@users.noreply.github.com>
2025-12-17 10:31:55 -0700

    Add ability to read and store channel metadata into an OSDF container (#1593)
    * WIP: Accept 1D variables with channel dimension in loadObsBlockFromNetcdf.
    
    * Added capability to read (from NetCDF) and store channel metadata in an OSDF container.
    
    * Added a case with channel metadata to the test_ioda_obsspace_osdf ctest
    
    * Fixed typos in comments.
    
    * Added more channel selection cases to the test_ioda_obsspace_osdf ctest.

--- src/reader/load/loadObsFromNetcdf.cpp
+++ src/reader/load/loadObsFromNetcdf.cpp
@@ -55,12 +55,15 @@ static bool isNetcdfVarADimension(const netCDF::NcGroup & group, const std::stri
 
 /// \brief check if we can keep a variable for loading into the OSDF container
 /// \details For now, the storage capability of the OSDF container is limited to
-/// 1D variables dimensioned by Location, or 2D variables dimensioned by Location and
-/// Channel. This function will check for this and return true if the variable can
-/// be loaded into the OSDF.
+/// 1D variables dimensioned by Location or Channel, or 2D variables dimensioned
+/// by Location and Channel. This function will check for this and return true
+/// if the variable can be loaded into the OSDF.
 /// \param varName hierarchical variable name
 /// \param var netCDF variable to check
-static bool keepNetcdfVarForOSDF(const std::string & varName, const netCDF::NcVar & var);
+/// \param varDimNames list in order of dimension names for the variable
+static bool keepNetcdfVarForOSDF(const std::string & varName,
+                                 const netCDF::NcVar & var,
+                                 std::vector<std::string> & varDimNames);
 
 /// \brief list out variable names in a recursive fashion
 /// \details This function will traverse the group hierarchy and return a vector
@@ -99,22 +102,39 @@ static netCDF::NcVar openHierarchialNetcdfVar(netCDF::NcGroup & topGroup,
 /// \param locCount count of locations to read
 /// \param var netcdf variable
 template <typename VarType>
-static std::vector<VarType> getSelectNcVarData(const std::size_t startLoc,
-                                               const std::size_t locCount,
-                                               const netCDF::NcVar & var);
+static std::vector<VarType> getNcVarData(const std::size_t startLoc,
+                                         const std::size_t locCount,
+                                         const netCDF::NcVar & var);
 
-/// \brief helper function for the getSelectNcVarData function
-/// \details This function is used by the getSelectNcVarData to make the call to
-/// the netCDF API to get the variable data. The function is specialized for
-/// std::string to handle the special case of string data in netCDF.
+/// \brief helper function for the getNcVarData function
+/// \details This function is used by the getNcVarData to make the call to
+/// the netCDF API to get the variable data when that variable has
+/// Location for the first dimenstion. In this case, we want to select
+/// a subset block of locations from the input file. The function is
+/// specialized for std::string to handle the special case of string
+/// data in netCDF.
 /// \tparam VarType data type of the variable
 /// \param var netcdf variable
 /// \param start starting location for the data
 /// \param count count of locations to read
 /// \param varData vector to hold the variable data
 template <typename VarType>
-static void getNcVarData(const netCDF::NcVar & var, const std::vector<std::size_t> & start,
-                         const std::vector<std::size_t> & count, std::vector<VarType> & varData);
+static void getSelectNcVarData(const netCDF::NcVar & var,
+                               const std::vector<std::size_t> & start,
+                               const std::vector<std::size_t> & count,
+                               std::vector<VarType> & varData);
+
+/// \brief helper function for the getNcVarData function
+/// \details This function is used by the getNcVarData to make the call to
+/// the netCDF API to get the variable data when that variable does not
+/// have Location for the first dimenstion. In this case, we want to
+/// read the entire variable from the file. The function is specialized
+/// for std::string to handle the special case of string data in netCDF.
+/// \tparam VarType data type of the variable
+/// \param var netcdf variable
+/// \param varData vector to hold the variable data
+template <typename VarType>
+static void getAllNcVarData(const netCDF::NcVar & var, std::vector<VarType> & varData);
 
 /// \brief replace fill values with JEDI missing values
 /// \param var netCDF variable
@@ -145,6 +165,7 @@ std::string getNcVarDtEpochString(const netCDF::NcVar & var);
 /// \param varName hierarchical name of variable
 /// \param varData variable data
 /// \param chanNums channel numbers
+/// \param varDimNames variable dimension names
 /// \param destOSDF destination OSDF container
 /// \param osdfMetadata frame metadata for dest OSDF
 template <typename VarType>
@@ -152,6 +173,7 @@ static void transferVarDataToOSDF(const std::string & varName,
                                   const std::vector<VarType> & varData,
                                   const std::size_t numlocs,
                                   const std::vector<int> & chanNums,
+                                  const std::vector<std::string> & varDimNames,
                                   std::unique_ptr<osdf::IFrame> & destOSDF,
                                   osdf::FrameMetadata & osdfMetadata);
 
@@ -190,28 +212,40 @@ bool isNetcdfVarADimension(const netCDF::NcGroup & group, const std::string & va
 // TODO(srh): We eventually need to eliminate the variable limitations on storage
 // in the OSDF container. For now, we can make a lot of progress before these limitations
 // are removed.
-bool keepNetcdfVarForOSDF(const std::string & varName, const netCDF::NcVar & var) {
+bool keepNetcdfVarForOSDF(const std::string & varName,
+                          const netCDF::NcVar & var,
+                          std::vector<std::string> & varDimNames) {
   // Get a list of the dimensions attached to this variable. For now we want to keep
-  // only the variables that are dimensioned by Location, or dimensioned by Location and
-  // Channel.
+  // variables that are dimensioned by:
+  //    1D: Location
+  //    1D: Channel
+  //    2D: Location, Channel
   bool keepVar = true;
   const std::vector<netCDF::NcDim> varDims = var.getDims();
   const std::size_t numDims = varDims.size();
+  varDimNames.clear();
   if ((numDims == 0) || (numDims > 2)) {
     oops::Log::info() << "WARNING: ioda::reader::keepNetcdfVarForOSDF: Variable: " << varName
               << " has no dimensions or more than 2 dimensions. Skipping." << std::endl;
 
     keepVar = false;
   } else if (numDims == 1) {
-    // 1D variable, check if it is dimensioned by Location
-    if (varDims[0].getName() != "Location") {
+    // 1D variable, check if it is dimensioned either by Location or by Channel
+    if (varDims[0].getName() == "Location") {
+      varDimNames.push_back("Location");
+    } else if (varDims[0].getName() == "Channel") {
+      varDimNames.push_back("Channel");
+    } else {
       oops::Log::info() << "WARNING: ioda::reader::keepNetcdfVarForOSDF: 1D Variable: " << varName
-                << " is not dimensioned by Location. Skipping." << std::endl;
+                << " is not dimensioned by either Location or Channel. Skipping." << std::endl;
       keepVar = false;
     }
   } else {
     // 2D variable, check if it is dimensioned by Location and Channel
-    if ((varDims[0].getName() != "Location") || (varDims[1].getName() != "Channel")) {
+    if ((varDims[0].getName() == "Location") && (varDims[1].getName() == "Channel")) {
+      varDimNames.push_back("Location");
+      varDimNames.push_back("Channel");
+    } else {
       oops::Log::info() << "WARNING: ioda::reader::keepNetcdfVarForOSDF: 2D Variable: " << varName
                 << " is not dimensioned by Location and Channel. Skipping." << std::endl;
       keepVar = false;
@@ -290,45 +324,74 @@ netCDF::NcVar openHierarchialNetcdfVar(netCDF::NcGroup & topGroup,
 
 //--------------------------------------------------------------------------------
 template <typename VarType>
-std::vector<VarType> getSelectNcVarData(const std::size_t startLoc, const std::size_t locCount,
-                                        const netCDF::NcVar & var) {
-  // Location is the first dimeension in this form of reading. We want to use the hyperslab
-  // selection for the block of locations (first dimension) and select the entirety of any
-  // remaining dimesions.
-  std::vector<size_t> count(0);
-  const std::vector<netCDF::NcDim> varDims = var.getDims();
-  for (auto & dim : varDims) {
-    count.push_back(dim.getSize());
-  }
-  std::vector<size_t> start(count.size(), 0);
-  start[0] = startLoc;
-  count[0] = locCount;
+std::vector<VarType> getNcVarData(const std::size_t startLoc, const std::size_t locCount,
+                                  const netCDF::NcVar & var) {
+  std::vector<VarType> varData;
+  if (var.getDim(0).getName() == "Location") {
+    // Location is the first dimension in this form of reading. We want to use the hyperslab
+    // selection for the block of locations (first dimension) and select the entirety of any
+    // remaining dimesions.
+    std::vector<size_t> count(0);
+    const std::vector<netCDF::NcDim> varDims = var.getDims();
+    for (auto & dim : varDims) {
+      count.push_back(dim.getSize());
+    }
+    std::vector<size_t> start(count.size(), 0);
+    start[0] = startLoc;
+    count[0] = locCount;
 
-  std::size_t numElements = 1;
-  for (auto & dimSize : count) {
-    numElements *= dimSize;
+    std::size_t numElements = 1;
+    for (auto & dimSize : count) {
+      numElements *= dimSize;
+    }
+    varData.resize(numElements);
+    getSelectNcVarData<VarType>(var, start, count, varData);
+  } else {
+    // Location is not the first dimension, so we read the entire variable.
+    varData.resize(var.getDim(0).getSize());
+    getAllNcVarData<VarType>(var, varData);
   }
-  std::vector<VarType> varData(numElements);
-  getNcVarData<VarType>(var, start, count, varData);
   return varData;
 }
 
 //--------------------------------------------------------------------------------
 template <typename VarType>
-void getNcVarData(const netCDF::NcVar & var, const std::vector<std::size_t> & start,
-                  const std::vector<std::size_t> & count, std::vector<VarType> & varData) {
+void getSelectNcVarData(const netCDF::NcVar & var,
+                        const std::vector<std::size_t> & start,
+                        const std::vector<std::size_t> & count,
+                        std::vector<VarType> & varData) {
   var.getVar(start, count, varData.data());
 }
 
 // Explicit specialization for std::string
 template <>
-void getNcVarData<std::string>(const netCDF::NcVar & var, const std::vector<std::size_t> & start,
-                   const std::vector<std::size_t> & count, std::vector<std::string> & varData) {
+void getSelectNcVarData<std::string>(const netCDF::NcVar & var,
+                                     const std::vector<std::size_t> & start,
+                                     const std::vector<std::size_t> & count,
+                                     std::vector<std::string> & varData) {
   // The string type is a special case where the data in the netCDF variable is stored in
   // allocated (heap) memory. The means for accessing this data a vector of char *
   // pointers must be used. Then the data is transferred to a vector of std::string.
   std::vector<char *> tmpVarData(varData.size());
   var.getVar(start, count, tmpVarData.data());
+  for (std::size_t i = 0; i < varData.size(); ++i) {
+    varData[i] = std::string(tmpVarData[i]);
+  }
+}
+
+//--------------------------------------------------------------------------------
+template <typename VarType>
+void getAllNcVarData(const netCDF::NcVar & var, std::vector<VarType> & varData) {
+  var.getVar(varData.data());
+}
+
+template <>
+void getAllNcVarData(const netCDF::NcVar & var, std::vector<std::string> & varData) {
+  // The string type is a special case where the data in the netCDF variable is stored in
+  // allocated (heap) memory. The means for accessing this data a vector of char *
+  // pointers must be used. Then the data is transferred to a vector of std::string.
+  std::vector<char *> tmpVarData(varData.size());
+  var.getVar(tmpVarData.data());
   for (std::size_t i = 0; i < varData.size(); ++i) {
     varData[i] = std::string(tmpVarData[i]);
   }
@@ -438,32 +501,70 @@ void transferVarDataToOSDF(const std::string & varName,
                            const std::vector<VarType> & varData,
                            const std::size_t numLocs,
                            const std::vector<int> & chanNums,
+                           const std::vector<std::string> & varDimNames,
                            std::unique_ptr<osdf::IFrame> & destOSDF,
                            osdf::FrameMetadata & osdfMetadata) {
-  // varData should be the proper size. It is either 1D (Location)
-  // or 2D (Location X Channel). For the 2D variable, the number of
-  // channels can be inferred from the chanNums vector.
+  // varData should be the proper size. It is either 1D (Location),
+  // or 1D (Channel) or or 2D (Location X Channel). We've already
+  // verified the dimensioning when we decided to keep the variable
+  // for loading into the OSDF container.
   const std::size_t numChannels = chanNums.size();
-  if (varData.size() == numLocs) {
-    // 1D variable, ready to append to the OSDF container
-    destOSDF->appendNewColumn(varName, varData);
-  } else if (varData.size() == (numLocs * numChannels)) {
-    // 2D variable, expand into one column per channel
-    for (std::size_t ichan = 0; ichan < numChannels; ++ichan) {
-      std::vector<VarType> dataChannel(numLocs);
-      for (std::size_t iloc = 0; iloc < numLocs; ++iloc) {
-        const std::size_t idata = (numChannels * iloc) + ichan;
-        dataChannel[iloc] = varData[idata];
+  if (varDimNames.size() == 1) {
+    if (varDimNames[0] == "Location") {
+      // 1D variable dimensioned by Location
+      if (varData.size() == numLocs) {
+        // Ready to append to the OSDF container
+        destOSDF->appendNewColumn(varName, varData);
+      } else {
+        const std::string errMsg = std::string("ioda::reader::transferVarDataToOSDF: ") +
+            std::string("1D Variable (Location) size != numLocs: ") + varName;
+        throw std::runtime_error(errMsg);
       }
-      destOSDF->appendNewColumn(
-        varName + std::string("_") + std::to_string(chanNums[ichan]),
-        dataChannel);
+    } else if (varDimNames[0] == "Channel") {
+      // 1D variable dimensioned by Channel
+      if (varData.size() == numChannels) {
+        // Expand into one column per channel. Do this by looping through the channels
+        // and creating a vector that is numLocs in length with the specific channel value
+        // repeated for all locations.
+        // TODO(srh): This is inefficient in terms of memory usage. We can address
+        // this later if it turns out to be problematic.
+        for (std::size_t ichan = 0; ichan < numChannels; ++ichan) {
+          std::vector<VarType> dataChannel(numLocs, varData[ichan]);
+          destOSDF->appendNewColumn(
+            varName + std::string("_") + std::to_string(chanNums[ichan]),
+            dataChannel);
+        }
+        osdfMetadata.addVarToVarsWithChans(varName);
+      } else {
+        const std::string errMsg = std::string("ioda::reader::transferVarDataToOSDF: ") +
+            std::string("1D Variable (Channel) size != numChannels: ") + varName;
+        throw std::runtime_error(errMsg);
+      }
     }
-    osdfMetadata.addVarToVarsWithChans(varName);
-  } else {
-    // The size of the variable data is not correct. This should never happen.
-    throw std::runtime_error(
-      "ioda::reader::transferVarDataToOSDF: Variable data size is not correct.");
+  } else if (varDimNames.size() == 2) {
+    // 2D variable, check if it is dimensioned by Location and Channel
+    if ((varDimNames[0] == "Location") && (varDimNames[1] == "Channel")) {
+      // 2D variable dimensioned by Location and Channel
+      if (varData.size() == (numLocs * numChannels)) {
+        // Expand into one column per channel
+        for (std::size_t ichan = 0; ichan < numChannels; ++ichan) {
+          std::vector<VarType> dataChannel(numLocs);
+          for (std::size_t iloc = 0; iloc < numLocs; ++iloc) {
+            const std::size_t idata = (numChannels * iloc) + ichan;
+            dataChannel[iloc] = varData[idata];
+          }
+          destOSDF->appendNewColumn(
+            varName + std::string("_") + std::to_string(chanNums[ichan]),
+            dataChannel);
+        }
+        osdfMetadata.addVarToVarsWithChans(varName);
+      } else {
+        const std::string errMsg = std::string("ioda::reader::transferVarDataToOSDF: ") +
+          std::string("2D Variable (Location, Channel) size != (numLocs * numChans): ") +
+          varName;
+        throw std::runtime_error(errMsg);
+      }
+    }
   }
 
   // Count variable (in numVars) if it is in the ObsValue group
@@ -524,30 +625,31 @@ int loadObsBlockFromNetcdf(netCDF::NcFile & inFile,
       osdfMetadata.setDateTimeEpoch(getNcVarDtEpochString(var));
     }
 
-    if (keepNetcdfVarForOSDF(varName, var)) {
+    std::vector<std::string> varDimNames;
+    if (keepNetcdfVarForOSDF(varName, var, varDimNames)) {
       // Get the variable type for transferring its data to the OSDF
       const netCDF::NcType varType = var.getType();
       if (varType.getName() == "int") {
-        std::vector<int> varData = getSelectNcVarData<int>(startLoc, locCount, var);
+        std::vector<int> varData = getNcVarData<int>(startLoc, locCount, var);
         replaceFillValuesWithMissing<int>(var, varData);
         transferVarDataToOSDF<int>(
-          varName, varData, locCount, chanNums, destOSDF, osdfMetadata);
+          varName, varData, locCount, chanNums, varDimNames, destOSDF, osdfMetadata);
       } else if (varType.getName() == "int64") {
-        std::vector<int64_t> varData = getSelectNcVarData<int64_t>(startLoc, locCount, var);
+        std::vector<int64_t> varData = getNcVarData<int64_t>(startLoc, locCount, var);
         replaceFillValuesWithMissing<int64_t>(var, varData);
         transferVarDataToOSDF<int64_t>(
-          varName, varData, locCount, chanNums, destOSDF, osdfMetadata);
+          varName, varData, locCount, chanNums, varDimNames, destOSDF, osdfMetadata);
       } else if (varType.getName() == "float") {
-        std::vector<float> varData = getSelectNcVarData<float>(startLoc, locCount, var);
+        std::vector<float> varData = getNcVarData<float>(startLoc, locCount, var);
         replaceFillValuesWithMissing<float>(var, varData);
         transferVarDataToOSDF<float>(
-          varName, varData, locCount, chanNums, destOSDF, osdfMetadata);
+          varName, varData, locCount, chanNums, varDimNames, destOSDF, osdfMetadata);
       } else if (varType.getName() == "string") {
         std::vector<std::string> varData =
-                                 getSelectNcVarData<std::string>(startLoc, locCount, var);
+                                 getNcVarData<std::string>(startLoc, locCount, var);
         replaceFillValuesWithMissing<std::string>(var, varData);
         transferVarDataToOSDF<std::string>(
-          varName, varData, locCount, chanNums, destOSDF, osdfMetadata);
+          varName, varData, locCount, chanNums, varDimNames, destOSDF, osdfMetadata);
       } else {
         oops::Log::info() << "WARNING: ioda::reader::loadObsBlockFromNetcdf: Variable: "
                           << varName << " is not int, int64, float or string. Skipping."
